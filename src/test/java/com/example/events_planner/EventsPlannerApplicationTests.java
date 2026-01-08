2aa3dbeddf77e0f73ef941735d0b61f035fb0ee0
Lars Maechel <maechel@users.noreply.github.com>
2026-01-08 15:04:42 +0100

    chore: initial commit

---
+++ src/test/java/com/example/events_planner/EventsPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.events_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
